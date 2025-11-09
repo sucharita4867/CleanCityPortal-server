bbd045b1be916597c1b0720ebec3240d5a79d694
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-09 21:27:50 +0530

    Implemented GET route and successfully fetched data from the server

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://CleanCityPortalDB:DIKZ1iaVKnMFaK8e@cluster0.phhktud.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("issues-db");
+    const issuesCollection = db.collection("issues");
+
+    app.get("/issues", async (req, res) => {
+      const result = await issuesCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //     await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
